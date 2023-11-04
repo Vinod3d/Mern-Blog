50bdaed5655951051faef333edfd0f2a2d8c3cca
Vinod Chandra <98041742+Vinod3d@users.noreply.github.com>
2023-11-04 12:05:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,41 @@
 require('dotenv').config();
-require('./routes/conn');
-const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const cookieParser = require('cookie-parser');
+require('./routes/conn')
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const cors = require('cors')
+const multer=require("multer")
+const cookieParser=require('cookie-parser')
 const fs = require('fs');
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
-const userRoute = require('./routes/users');
-const commentRoute = require('./routes/comments');
+const authRoute = require('./routes/auth')
+const postRoute=require('./routes/posts')
+const userRoute=require('./routes/users')
+const commentRoute=require('./routes/comments');
 const path = require('path');
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser();
-app.use('/images', express.static(path.join(__dirname, '/images')));
 
-// Set up CORS for your serverless functions
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mern-blogs-app.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
-// Define your serverless function for handling requests
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
-app.use('/api/comments', commentRoute);
 
+app.use(express.json())
+// app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.use(cookieParser())
+app.use("/images",express.static(path.join(__dirname,"/images")))
+app.use("/api/auth", authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/comments",commentRoute)
+
+//image upload
 // Create the "images" directory if it doesn't exist
 const imagesDirectory = 'images';
 if (!fs.existsSync(imagesDirectory)) {
@@ -55,11 +61,9 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
         console.error("Error while uploading:", error);
         res.status(500).json("Image upload failed");
     }
-});
+}); 
+  
 
-module.exports = app; // Export the Express app
-
-// Vercel-specific code to handle serverless functions
-module.exports.handler = (req, res) => {
-    app(req, res);
-};
+app.listen(process.env.PORT, ()=>{
+    console.log("server is running on port " +  process.env.PORT);
+})
