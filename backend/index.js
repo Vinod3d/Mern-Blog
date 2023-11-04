c51681d73fb5c1aac8c8130b8d314247acd0e60a
Vinod Chandra <98041742+Vinod3d@users.noreply.github.com>
2023-11-04 16:59:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
-require('./routes/conn')
-const express = require('express')
-const app = express()
-const mongoose = require("mongoose")
-const cors = require('cors')
-const multer=require("multer")
-const cookieParser=require('cookie-parser')
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
-const authRoute = require('./routes/auth')
-const postRoute=require('./routes/posts')
-const userRoute=require('./routes/users')
-const commentRoute=require('./routes/comments');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const userRoute = require('./routes/users');
+const commentRoute = require('./routes/comments');
 const path = require('path');
+const serverless = require('serverless-http');
 
+const app = express();
+const router = express.Router();
 
+app.use(express.json());
+app.use(cors({ origin: 'https://mern-blogs-app.vercel.app', credentials: true }));
+app.use(cookieParser();
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
+// Define your routes on the router
+router.use('/api/auth', authRoute);
+router.use('/api/users', userRoute);
+router.use('/api/posts', postRoute);
+router.use('/api/comments', commentRoute);
 
-app.use(express.json())
-app.use(cors({origin:"https://mern-blogs-app.vercel.app",credentials:true}))
-app.use(cookieParser())
-app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use("/api/auth", authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/posts",postRoute)
-app.use("/api/comments",commentRoute)
+app.use('/.netlify/functions/api', router); // This is the base path for Vercel serverless functions
 
-//image upload
-// Create the "images" directory if it doesn't exist
+// Image upload
 const imagesDirectory = 'images';
 if (!fs.existsSync(imagesDirectory)) {
     fs.mkdirSync(imagesDirectory);
@@ -34,7 +34,7 @@ if (!fs.existsSync(imagesDirectory)) {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, imagesDirectory); // Use the existing or newly created "images" directory
+        cb(null, imagesDirectory);
     },
     filename: (req, file, cb) => {
         cb(null, req.body.img);
@@ -46,14 +46,14 @@ const upload = multer({ storage: storage });
 app.post('/api/upload', upload.single('file'), (req, res) => {
     try {
         // Your file upload logic here
-        res.status(200).json("Image has been uploaded successfully");
+        res.status(200).json('Image has been uploaded successfully');
     } catch (error) {
-        console.error("Error while uploading:", error);
-        res.status(500).json("Image upload failed");
+        console.error('Error while uploading:', error);
+        res.status(500).json('Image upload failed');
     }
-}); 
-  
+});
 
-app.listen(process.env.PORT, ()=>{
-    console.log("server is running on port " +  process.env.PORT);
-})
+module.exports = app; // Export the Express app
+
+// Vercel-specific code to handle serverless functions
+module.exports.handler = serverless(app);
