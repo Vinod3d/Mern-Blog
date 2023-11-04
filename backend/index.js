3f1625eeb3b5f125b0188c31ef7f2ba8fc12d767
Vinod Chandra <98041742+Vinod3d@users.noreply.github.com>
2023-11-04 16:54:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const path = require('path');
 
 
 app.use(express.json())
-// app.use(cors({ origin: "https://mern-blogs-app.vercel.app", credentials: true }));
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use("/api/auth", authRoute)
